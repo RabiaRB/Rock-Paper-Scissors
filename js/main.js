81730c64321513effc7c3210dad928208b4dadcd
Rabiya Butt <air.bay.x@gmail.com>
2023-02-21 11:06:03 +0500

    changes in loops

--- js/main.js
+++ js/main.js
@@ -1,31 +1,40 @@
 // what do we want to do???
 
 // - display round Number
+let round;
+let userChoice = '';
+let userPoints = 0;
+let computerChoice = '';
+let computerPoints = 0;
+let totalPoints = userPoints + computerPoints
 
-let userChoice;
-let userPoints;
-let computerChoice;
-let computerPoints;
-
-for (let i = 1; i <= 10; i++) {
-    document.querySelector('#roundNum').innerHTML = i;
-    
+do {
+    document.querySelector('#roundNum').innerHTML = 1;
     generateUser();
-    generateComputer();
-    playGame();
     displayResult();
+} while (round >= 10 && (totalPoints == 10));
 
-}
+// document.querySelector('button').setAttribute("disabled");
+
+// document.querySelector('#resetGame').addEventListener('click', reset);
+
+// function reset() {
+//     document.querySelector('button').removeAttribute("disabled");
+    
+// }
+
     // document.querySelector('#gameOver').
         
     function displayResult() {
         if (userPoints > computerPoints) {
             document.querySelector('#winner').innerText = "YOU ARE THE WINNER";
-        } else if (userPoints == computerPoints) {
-            document.querySelector('#winner').innerText = "YOU HAVE A TIE";
-        } else {
+        } else if (userPoints < computerPoints) {
             document.querySelector('#winner').innerText = "THE COMPUTER WON THE GAME";
+        } else {
+            document.querySelector('#winner').innerText = "YOU HAVE A TIE";
+;
         }
+        round++
     }
 
 
@@ -43,45 +52,56 @@ function generateUser() {
     function userRock() {
         userChoice = "Rock";
         document.querySelector('#userChoice').innerText = userChoice;
+        generateComputer();
     }
 
     function userPaper() {
         userChoice = "Paper";
         document.querySelector('#userChoice').innerText = userChoice;
+        generateComputer();
     }
 
     function userScissors() {
         userChoice = "Scissors";
         document.querySelector('#userChoice').innerText = userChoice;
+        generateComputer();
     }
 
 }
 // - generate choice for computer and display
 function generateComputer() {
     
-    let randomComputerChoice = Math.random();
-    if (randomComputerChoice < 0.3) {
-        computerChoice = "Rock";
-    } else if (randomComputerChoice < 0.6) {
-        computerChoice = "Paper";
-    } else {
-        computerChoice = "Scissors";
+    let randomComputerChoice = Math.floor(Math.random()*3);
+    switch (randomComputerChoice) {
+        case 0:
+            computerChoice = 'Rock';
+            break;
+        case 1:
+            computerChoice = 'Paper';
+            break;
+        case 2:
+            computerChoice = 'Scissors';
+            break;
     }
+
     document.querySelector('#computerChoice').innerText = computerChoice;
+    playGame();
 }
 // - compare choice of user with computer
     
 function playGame() {
     if ((userChoice === 'Scissors' && computerChoice === 'Paper') || (userChoice === 'Paper' && computerChoice === 'Rock') || (userChoice === 'Rock' && computerChoice === 'Scissors')){
-        userPoints++;
-    } else if (userChoice == computerChoice) {
-        
+        userPoints += 1;
+        document.querySelector('#userPoints').innerText = userPoints;
+    } else if (userChoice === computerChoice) {
+        console.log('you have same choice')
     } else {
-        computerPoints++;
+        computerPoints += 1;
+        document.querySelector('#computerPoints').innerText = computerPoints;
     }
-    document.querySelector('#userPoints').innerText = userPoints;
-    document.querySelector('#computerPoints').innerText = computerPoints;
- 
+    
+    // console.log(`user choice: ${userChoice} and computer choice: ${computerChoice}`)
+    
 }
 
 // - determine the winner and add points to their score
